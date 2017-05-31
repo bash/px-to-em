21bf60fa10d3e9fb147a6e57495352f1b9b9c35a
Samuel Weiss <samuel@hausformat.com>
2017-05-31 12:43:30 +0200

    Add lineheight calculation

--- devtools.js
+++ devtools.js
@@ -5,17 +5,21 @@ var getProperties = function() {
 
   const computedStyle = window.getComputedStyle($0)
   const fontSize = Number.parseFloat(computedStyle.fontSize)
+  const lineHeight = Number.parseFloat(computedStyle.lineHeight)
 
   const parentStyle = window.getComputedStyle($0.parentNode)
   const baseFontSize = Number.parseFloat(parentStyle.fontSize)
 
   const emSize = Math.round(fontSize / baseFontSize * 10000) / 10000
+  const emLineHeight = Math.round(lineHeight / fontSize * 10000) / 10000
   const result = Object.create(null)
 
   result.$0 = $0
   result.fontSizePx = `${fontSize}px`
   result.baseFontSize = `${baseFontSize}px`
   result.fontSizeEm = `${emSize}em`
+  result.lineHeight = `${lineHeight}px`
+  result.lineHeightEm = `${emLineHeight}em`
 
   return result
 }
